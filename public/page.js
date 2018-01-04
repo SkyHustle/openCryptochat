eb97002f77c366a8bf988e845552679930d0b5e4
Dmitry Sky <dmitry.sky@icloud.com>
2018-01-04 14:32:52 -0700

    setup client side socket listeners

--- public/page.js
+++ public/page.js
@@ -15,9 +15,53 @@ const vm = new Vue ({
     }
   },
   created () {
-    this.addNotification('Hello World')
+    // Initialize socket.io
+    this.socket = io()
+    this.setupSocketListeners()
   },
   methods: {
+    setupSocketListeners () {
+      // Automatically join default room on connect
+      this.socket.on('connect', () => {
+        this.addNotification('Connected To Server.')
+        this.joinRoom()
+      })
+
+      // Notify user that they have lost the socket connection
+      this.socket.on('disconnect', () => this.addNotification('Lost Connection'))
+
+      // Display message when recieved
+      this.socket.on('MESSAGE', (message) => {
+        this.addMessage(message)
+      })
+    },
+
+    /** Send the current draft message */
+    sendMessage () {
+      // Don't send message if there is nothing to send
+      if (!this.draft || this.draft === '') { return }
+
+      const message = this.draft
+
+      // Reset the UI input draft text
+      this.draft = ''
+
+      // Instantly add message to local UI
+      this.addMessage(message)
+
+      // Emit the message
+      this.socket.emit('MESSAGE', message)
+    },
+
+    /** Join the chatroom */
+    joinRoom () {
+      this.socket.emit('JOIN')
+    },
+
+    /** Add message to UI */
+    addMessage (message) {
+      this.messages.push(message)
+    },
     /** Append a notification message in the UI */
     addNotification (message) {
       const timestamp = new Date().toLocaleTimeString()
