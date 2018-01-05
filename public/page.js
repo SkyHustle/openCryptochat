0baa63ea02474b31de0c563dcdc5d75372977f07
Dmitry Sky <dmitry.sky@icloud.com>
2018-01-05 13:06:13 -0700

    congifure VueApp to communicate with webworker via page

--- public/page.js
+++ public/page.js
@@ -67,5 +67,30 @@ const vm = new Vue ({
       const timestamp = new Date().toLocaleTimeString()
       this.notifications.push({ message, timestamp })
     },
+    /** Post a message to the web worker and return a promise that will resolve with the response.  */
+    getWebWorkerResponse (messageType, messagePayload) {
+      return new Promise((resolve, reject) => {
+        // Generate a random message id to identify the corresponding event callback
+        const messageId = Math.floor(Math.random() * 100000)
+
+        // Post the message to the webworker
+        this.cryptWorker.postMessage([messageType, messageId].concat(messagePayload))
+
+        // Create a handler for the webworker message event
+        const handler = function (e) {
+          // Only handle messages with the matching message id
+          if (e.data[0] === messageId) {
+            // Remove the event listener once the listener has been called.
+            e.currentTarget.removeEventListener(e.type, handler)
+
+            // Resolve the promise with the message payload.
+            resolve(e.data[1])
+          }
+        }
+
+        // Assign the handler to the webworker 'message' event.
+        this.cryptWorker.addEventListener('message', handler)
+      })
+    }
   }
 })
